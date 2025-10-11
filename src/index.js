ebf8a05e1794f2afcbda260d077615e31ddf66d6
Giovanni Buoli <giovannibuoli@gmail.com>
2025-10-11 18:31:10 +0200

    debug: solo testo Convocalcio, senza NotificationProvider

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import ConvoCalcio from './ConvoCalcio';
-import { NotificationProvider } from './NotificationContext'; // Import obbligatorio!
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <NotificationProvider>
-    <ConvoCalcio />
-  </NotificationProvider>
-);
+root.render(<ConvoCalcio />);
